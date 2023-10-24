0075eaeca1b86656fc203e38050eb84295cf8089
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2023-10-24 10:30:52 +0700

    UI rekomendasi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/rekomendasi", methods=["GET", "POST"])
+def rekomendasi():
+    if request.method == "POST":
+        return render_template("hasil_rekomendasi.html")
+    else:
+        return render_template("rekomendasi.html")
 
 
 if __name__ == "__main__":
